04fe7c9bbcfe669860c45da7c90772c90a8ce9e8
BooleanCube <47650058+BooleanCube@users.noreply.github.com>
2022-12-02 14:57:49 -0500

    day 2 part 1

--- D2P1.py
+++ D2P1.py
@@ -1,15 +1,10 @@
 score = {"X":1, "Y":2, "Z":3}
-value = {"A":1, "B":2, "C":3}
-su = 0
+win = ['A Y', 'B Z', 'C X']
+draw = ['A X', 'B Y', 'C Z']
+s = 0
 while 1:
     inp = input()
     if inp == "done":break
-    l = inp.split()
-    c = 0
-    s = value[l[0]]
-    e = score[l[1]]
-    c += e
-    if e>s or s-e==2: c += 6
-    elif s==e: c += 3
-    su += c
-print(su)
+    s += 6 if inp in win else 3 if inp in draw else 0
+    s += score[inp.split()[1]]
+print(s)
